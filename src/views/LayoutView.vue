859b53851cce81f0d1f74c57a8e3026e4c0fe010
Cheng07-python <71380739+Cheng07-python@users.noreply.github.com>
2022-03-13 16:29:50 +0800

    Update LayoutView.vue

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -16,17 +16,17 @@
     <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
   </el-row>
     -->
-  <el-row class="row-bg" justify="space-around">
+  <el-row justify="space-around">
     <el-col :span="5">
       <el-card class="box-card">
         <template #header>
           <div class="card-header">
-            <span>Card name</span>
-            <el-button class="button" type="text">Operation button</el-button>
+            <h2>{{ username }}</h2>
           </div>
         </template>
-        <div v-for="o in 4" :key="o" class="text item">
-          {{ "List item " + o }}
+        <div>
+          <div>ID:{{ id }} | {{ grade }} | {{ sex }}</div><br>
+          <div><b>认证:{{ tags }}</b></div><br>
         </div>
       </el-card>
 
@@ -43,15 +43,19 @@
           {{ item }}
         </p>
       </el-scrollbar>
-      
-      <el-pagination background layout="prev, pager, next" :total="1500">
+
+      <el-pagination
+        background
+        layout="prev, pager, next, jumper"
+        :total="200"
+        :pager-count="9"
+        :page-size="20"
+        :hide-on-single-page="true"
+      >
       </el-pagination>
     </el-col>
 
     <!--<el-col :span="6"><div class="grid-content bg-purple"></div></el-col>-->
-    
-       
-    
   </el-row>
 </template>
 
@@ -97,3 +101,17 @@
   color: var(--el-color-primary);
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+        username: "test",
+        id: "1",
+        grade: "九年级",
+        sex: "♂",
+        tags: "创始人"
+    };
+  },
+};
+</script>
