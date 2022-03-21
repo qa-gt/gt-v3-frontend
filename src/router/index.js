155633c689a33bf6fa072a860d61884fef66651f
Cheng07-python <cheng070225@sina.com>
2022-03-21 19:11:35 +0800

    redirect "/" to "/index"

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//import HomeView from '../views/HomeView.vue'
 
 const routes = [{
         path: '/',
         name: 'home',
-        component: HomeView
+        redirect: "/index",
     },
     {
         path: '/about',
