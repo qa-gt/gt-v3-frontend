892cb2fec20569a98a1173003b029dd22c5867e5
Danny-Yxzl <danny070601@aliyun.com>
2022-03-24 02:43:28 +0800

    [DELETE] Delete old.

--- src/views/WriteView.vue
+++ src/views/WriteView.vue
@@ -42,7 +42,7 @@
                         :left-toolbar="
                             isMobile
                                 ? 'undo redo | link image | save'
-                                : 'undo redo | h bold italic strikethrough quote | ul ol table hr | link image | save'
+                                : 'undo redo | h bold italic strikethrough quote emoji | ul ol table hr | link image | save'
                         "
                         :right-toolbar="
                             isMobile
@@ -58,44 +58,15 @@
 
                     <el-row gutter="20" justify="space-between">
                         <el-col>
-                            <!-- <el-upload
-                class="upload-demo"
-                action="http://127.0.0.1:5000/upload"
-                :on-change="handleChange"
-                :file-list="fileList"
-                :on-success="uploadSuccess"
-                :on-error="uploadError"
-                show-file-list="true"
-                multiple="true"
-                limit="2"
-                accept=".jpg, .jpeg, .png, .gif"
-                :style="{
-                  display: 'inline-block',
-                  width: isMobile ? '50%' : '0',
-                }"
-              >
-                <el-button type="primary"> 点我上传图片 </el-button>
-              </el-upload> -->
                             <el-select
                                 v-model="articleTopic"
                                 placeholder="请选择话题"
                             >
-                                <!-- <el-select
-                v-model="articleTopic"
-                placeholder="请选择话题"
-                :style="{
-                  float: 'right',
-                  width: isMobile ? '45%' : '25%',
-                  'min-width': '120px',
-                  'margin-right': isMobile ? '5%' : '55%',
-                }"
-              > -->
                                 <el-option
                                     key="item.value"
                                     label="item.lab21e212e1e12el"
                                     value="item.value"
-                                >
-                                </el-option>
+                                />
                             </el-select>
                         </el-col>
                     </el-row>
