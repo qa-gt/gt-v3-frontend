3fb4c7319860527d0a53cf2172bb3cb9a960df68
Cheng07-python <cheng070225@sina.com>
2022-03-25 16:48:08 +0800

    [UPDATE] Update PagenotfoundView.vue: Added SVG

--- src/views/PagenotfoundView.vue
+++ src/views/PagenotfoundView.vue
@@ -1,14 +1,26 @@
 <template>
-  <h1>Oops!</h1>
-  <h3>404 Page Not Found! </h3>
+  <div style="text-align: center;  justify-content: center; align-items: center">
+    <div class="oops">Oops!</div>
+    <br /><br />
+    <embed
+      src="https://iconfont.alicdn.com/s/0cf93df7-fb0a-4ac5-aba9-1310f7d4008b_origin.svg"
+      type="image/svg+xml"
+    /><br /><br />
+    <h3 class="pagenf">404 Page Not Found!</h3>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.oops {
+  color: #a5a5a5;
+  font-size: 55px;
+  font-weight: 70px;
+}
+.pagenf {
+  color: #c5c5c5;
+}
 </style>
