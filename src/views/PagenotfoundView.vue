71270bbabd0043a457524551f999858309ea61be
Cheng07-python <cheng070225@sina.com>
2022-03-25 16:57:38 +0800

    [UPDATE] Update 404.svg

--- src/views/PagenotfoundView.vue
+++ src/views/PagenotfoundView.vue
@@ -3,7 +3,7 @@
     <div class="oops">Oops!</div>
     <br /><br />
     <embed
-      src="https://iconfont.alicdn.com/s/0cf93df7-fb0a-4ac5-aba9-1310f7d4008b_origin.svg"
+      src="/src/assets/404.svg"
       type="image/svg+xml"
     /><br /><br />
     <h3 class="pagenf">404 Page Not Found!</h3>
