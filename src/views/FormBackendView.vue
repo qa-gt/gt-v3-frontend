b86c6439ac5f26c93ac55e2016bce6cca383d264
Cheng07-python <cheng0870225@sina.com>
2022-04-22 17:50:18 +0800

    [UPDATE] FormBackendView.vue

--- src/views/FormBackendView.vue
+++ src/views/FormBackendView.vue
@@ -12,24 +12,45 @@
     </el-col>
 
     <el-col :xs="24" :sm="18" :md="17" :lg="18" :xl="19">
-      <el-card>
-        <h2>创建一个表单</h2>
-        <el-divider />
+      <div style="margin-bottom: 15px">
         <el-card>
-          <el-dropdown>
-            <el-button type="primary" plain>
-              Dropdown List
-            </el-button>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item command="choice">单选题</el-dropdown-item>
-                <el-dropdown-item command="muiltichoice">多选题</el-dropdown-item>
-                <el-dropdown-item>填空题</el-dropdown-item>
-                <el-dropdown-item>时间选择</el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
+          <h2>创建一个表单</h2>
+          <el-divider />
+          <el-card>
+            <el-dropdown>
+              <el-button type="primary" plain> 添加组件 </el-button>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item command="Choice">单选题</el-dropdown-item>
+                  <el-dropdown-item command="MultipleChoice"
+                    >多选题</el-dropdown-item
+                  >
+                  <el-dropdown-item command="Filling">填空题</el-dropdown-item>
+                  <el-dropdown-item command="TimeChoice"
+                    >时间选择</el-dropdown-item
+                  >
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </el-card>
         </el-card>
+      </div>
+
+      <el-card>
+        <h2>表单预览</h2>
+        <el-divider />
+        <div :key="item" v-for="item in formitems" stlye="margin-bottom: 15px">
+          <div style="margin-bottom: 15px">
+            <el-card>
+              <template #header>
+                <div class="card-header">
+                  <h4>{{ item.title }}</h4>
+                </div>
+              </template>
+              <p>题目内容</p>
+            </el-card>
+          </div>
+        </div>
       </el-card>
     </el-col>
   </el-row>
@@ -45,6 +66,19 @@ export default {
   },
   components: {
     gtUser,
+  },
+  data() {
+    return {
+      formitems: [
+        { type: "单选", title: "请输入标题" },
+        { type: "单选", title: "请输入标题" },
+        { type: "单选", title: "请输入标题" },
+      ],
+    };
+  },
+  methods: {
+    Choice() {},
+    MultipleChoice() {},
   },
 };
 </script>
