d613ded7bf5a7e5909e30cc1bb6359647f93b525
Danny-Yxzl <danny070601@aliyun.com>
2022-04-27 15:19:18 +0800

    [UPDATE]

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -3,36 +3,48 @@
         <el-calendar ref="calendar" style="--el-calendar-cell-width: 115px">
             <template #header="{ date }" style="margin-bottom: initial">
                 <h3
-                    :style="{
-                        display: 'inline-block',
-                        width: '100%',
-                        'margin-bottom': '20px',
-                        'text-align': 'center',
-                    }"
+                    style="display: inline-block"
+                    :style="
+                        isMobile
+                            ? {
+                                  width: '100%',
+                                  'margin-bottom': '20px',
+                                  'text-align': 'center',
+                              }
+                            : { width: 'auto' }
+                    "
                 >
                     {{ date }}
                 </h3>
                 <el-button-group
-                    :style="{
-                        display: 'flex',
-                        'justify-content': 'center',
-                        width: '100%',
-                    }"
+                    :style="
+                        isMobile
+                            ? {
+                                  display: 'flex',
+                                  'justify-content': 'center',
+                                  width: '100%',
+                              }
+                            : {
+                                  width: 'auto',
+                                  display: 'inline-block',
+                                  float: 'right',
+                                  margin: '1rem 0',
+                              }
+                    "
                 >
-                    <el-button size="small" @click="selectDate('prev-year')">
-                        去年
-                    </el-button>
-                    <el-button size="small" @click="selectDate('prev-month')">
-                        上月
-                    </el-button>
-                    <el-button size="small" @click="selectDate('today')">
-                        今天
-                    </el-button>
-                    <el-button size="small" @click="selectDate('next-month')">
-                        下月
-                    </el-button>
-                    <el-button size="small" @click="selectDate('next-year')">
-                        明年
+                    <el-button
+                        v-for="i in [
+                            ['prev-year', '上年'],
+                            ['prev-month', '上月'],
+                            ['today', '今天'],
+                            ['next-month', '下月'],
+                            ['next-year', '下年'],
+                        ]"
+                        :key="i"
+                        :size="isMobile ? 'small' : 'default'"
+                        @click="selectDate(i[0])"
+                    >
+                        {{ i[1] }}
                     </el-button>
                 </el-button-group>
             </template>
@@ -50,33 +62,41 @@
                         height="75px"
                         style="margin-top: 5px"
                     >
-                        <el-tag
+                        <el-popover
                             v-for="(item, index) in dayEvents(data.day)"
                             :key="index"
-                            :type="item.type"
-                            style="
-                                margin: 1.5px;
-                                word-break: break-all;
-                                white-space: normal;
-                                height: auto;
-                                padding: 5px;
-                                display: inline-box;
-                            "
+                            placement="top-start"
+                            trigger="click"
                         >
-                            {{ item.content }}
-                        </el-tag>
+                            <template #reference>
+                                <el-tag :type="item.type" class="event">
+                                    {{ item.content }}
+                                </el-tag>
+                            </template>
+                            <el-popover-content>
+                                <h4 class="event-pop-title">
+                                    {{ item.content }}
+                                </h4>
+                                <small class="event-pop-date">
+                                    {{ item.date }}
+                                </small>
+                                <p class="event-pop-content">
+                                    {{ item.content }}
+                                </p>
+                            </el-popover-content>
+                        </el-popover>
                     </el-scrollbar>
                 </p>
             </template>
         </el-calendar>
-        <el-timeline style="margin-left: -10px">
+        <el-timeline style="margin-left: -10px" v-if="isMobile">
             <el-timeline-item
-                v-for="(activity, index) in activities"
-                :key="index"
-                :timestamp="activity.timestamp"
-                :type="activity.type"
+                v-for="event in dayEvents(currentDate)"
+                :key="event"
+                :timestamp="event.date"
+                :type="event.type"
             >
-                {{ activity.content }}
+                {{ event.content }}
             </el-timeline-item>
         </el-timeline>
     </el-card>
@@ -87,73 +107,53 @@ import dayjs from "dayjs";
 export default {
     data() {
         return {
-            selected: null,
-            calendar: {},
-            data: {
-                type: "today",
-                day: "",
-                isSelected: false,
-                date: "",
-            },
-            activities: [
-                {
-                    content: "Event start start start start start start ",
-                    timestamp: "2018-04-15",
-                    type: "success",
-                },
-                {
-                    content: "Approved",
-                    timestamp: "2018-04-13",
-                    type: "info",
-                },
-                {
-                    content: "Success",
-                    timestamp: "2018-04-11",
-                    type: "warning",
-                },
-            ],
+            currentDate: null,
             events: [
-                {
-                    date: "2022-04-26",
-                    content: "放假",
-                    type: "info",
-                },
+                { date: "2022-04-26", content: "放假", type: "info" },
                 { date: "2022-04-26", content: "考试", type: "success" },
-                { date: "2022-04-26", content: "上学", type: "warning" },
+                { date: "2022-04-27", content: "上学", type: "warning" },
                 { date: "2022-04-26", content: "搞事", type: "danger" },
             ],
         };
     },
     computed: {
-        isMobile: function () {
+        isMobile() {
             return this.$root.isMobile;
         },
     },
     methods: {
         dayEvents(day) {
-            let res = [];
-            for (let i in this.events) {
-                if (this.events[i].date === day) {
-                    res.push(this.events[i]);
-                }
+            if (!day) return [];
+            if (typeof day === "object") {
+                day = this.$moment(day.valueOf()).format("YYYY-MM-DD");
             }
-            return res;
+            return this.events.filter(event => {
+                return dayjs(event.date).isSame(day, "day");
+            });
         },
         selectDate(value) {
-            this.selected = this.selected || dayjs();
+            let date = this.$refs.calendar.date || dayjs();
             if (value === "prev-month") {
-                this.selected = this.selected.add(-1, "month");
+                date = date.add(-1, "month");
             } else if (value === "next-month") {
-                this.selected = this.selected.add(1, "month");
+                date = date.add(1, "month");
             } else if (value === "prev-year") {
-                this.selected = this.selected.add(-1, "year");
+                date = date.add(-1, "year");
             } else if (value === "next-year") {
-                this.selected = this.selected.add(1, "year");
+                date = date.add(1, "year");
             } else if (value === "today") {
-                this.selected = dayjs();
+                date = dayjs();
             }
-            this.$refs.calendar.pickDay(this.selected);
+            this.$refs.calendar.pickDay(date);
         },
+    },
+    created() {
+        setInterval(
+            () =>
+                (this.currentDate =
+                    (this.$refs.calendar && this.$refs.calendar.date) || null),
+            100
+        );
     },
 };
 </script>
@@ -169,5 +169,25 @@ export default {
 
 :deep(td.current > div > p > span) {
     font-weight: bold !important;
+}
+
+.event {
+    margin: 1.5px;
+    word-break: break-all;
+    white-space: normal;
+    height: auto;
+    padding: 5px;
+}
+.event-pop-title {
+    margin: 0;
+}
+.event-pop-content {
+    margin: 0;
+}
+.event-pop-date {
+    font-size: 12px;
+    color: #aeb8c6;
+    display: inline-block;
+    margin: 5px !important;
 }
 </style>
